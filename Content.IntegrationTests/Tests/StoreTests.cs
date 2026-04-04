203d1de1da6e95b77e4bcaa38aae15bfcf65e306
SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
2026-04-04 21:46:43 +0200

    Universal uplink codes (#38712)
    * Halfway commit
    
    * Finishing commit, maybe?
    
    * Fix test, update ringtone UI to look nicer
    
    * Fix command, add failsafe
    
    * Documentation
    
    * Can we just mark ValidatePrototypeId as obsolete please
    
    * I'm too tired and my bones hurt
    
    * Change uplink code generation method
    
    * Move RingerAccessUplinkComponent to Server, because cheat clients could make use of it
    
    * Fix uplink implant changes, review changes, repair broken serialization.
    
    * cleanup and master merge
    
    * forgot the Linq
    
    * Linqd
    
    * Our store system is pretty goddamn awful wow
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.IntegrationTests/Tests/StoreTests.cs
+++ Content.IntegrationTests/Tests/StoreTests.cs
@@ -24,7 +24,7 @@ public sealed class StoreTests
 - type: entity
   name: InventoryPdaDummy
   id: InventoryPdaDummy
-  parent: BasePDA
+  parent: [BasePDA, StorePresetUplink]
   components:
   - type: Clothing
     QuickEquip: false
@@ -78,10 +78,13 @@ public sealed class StoreTests
             mindSystem.TransferTo(mind, human, mind: mind);
 
             FixedPoint2 originalBalance = 20;
-            uplinkSystem.AddUplink(human, originalBalance, null, true);
+            uplinkSystem.AddUplink(human, originalBalance, out var notes, pda, null, true);
 
-            var storeComponent = entManager.GetComponent<StoreComponent>(pda);
-            var discountComponent = entManager.GetComponent<StoreDiscountComponent>(pda);
+            var remote = entManager.GetComponent<RemoteStoreComponent>(pda);
+            var storeEnt = remote.Store;
+            Assert.That(storeEnt.HasValue);
+            var storeComponent = entManager.GetComponent<StoreComponent>(storeEnt.Value);
+            var discountComponent = entManager.GetComponent<StoreDiscountComponent>(storeEnt.Value);
             Assert.That(
                 discountComponent.Discounts,
                 Has.Exactly(6).Items,
@@ -127,7 +130,7 @@ public sealed class StoreTests
                     Assert.That(plainDiscountedCost.Value, Is.LessThan(prototypeCost.Value), "Expected discounted cost to be lower then prototype cost.");
 
 
-                    var buyMsg = new StoreBuyListingMessage(discountedListingItem.ID){Actor = human};
+                    var buyMsg = new StoreBuyListingMessage(discountedListingItem.ID, null){Actor = human};
                     server.EntMan.EventBus.RaiseLocalEvent(pda, buyMsg);
 
                     var newBalance = storeComponent.Balance[UplinkSystem.TelecrystalCurrencyPrototype];
