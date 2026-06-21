cff214f2903106e9b8cf53ce6e2b75671faaf89d
nykaestra <verityofthestars@gmail.com>
2026-06-21 10:06:01 +1000

    disabilities courtesy of ECCO

---
+++ Content.Shared/_SpL/Traits/BrittleBonesComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._SpL.Traits;
+
+[RegisterComponent]
+public sealed partial class BrittleBonesComponent : Component
+{
+    // @screams! - RK
+}
---
+++ Resources/Locale/en-US/_SpL/Traits/quirks.ftl
@@ -0,0 +1,2 @@
+﻿trait-alcoholtolerance-name = Alcohol Tolerance
+trait-alcoholtolerance-desc = You could drink a whole field of horses under the table.
