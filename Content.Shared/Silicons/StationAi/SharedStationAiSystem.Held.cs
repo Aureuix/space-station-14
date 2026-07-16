abeda7a6014965a0b02e8dbff78b87f53b850e13
nykaestra <verityofthestars@gmail.com>
2026-07-17 08:23:26 +1000

    SL PR 5117 - station AI performance improvements

--- Content.Shared/Silicons/StationAi/SharedStationAiSystem.Held.cs
+++ Content.Shared/Silicons/StationAi/SharedStationAiSystem.Held.cs
@@ -187,7 +187,7 @@ public abstract partial class SharedStationAiSystem
         }
 
         // Starlight Start
-        if (_vision.IsOutsideCameraView(args.Target))
+        if (_vision.IsOutsideCameraViewCached(args.Target))
             return;
         // Starlight End
         var user = args.User;
