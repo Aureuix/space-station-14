cb36c82cfe03778d101a62656d65e2e2e1a245a3
Floximo <proximitron@gmail.com>
2026-09-05 18:42:10 +0000

    AI: Reconnect to last shunt (#5933)
    This simply adds a new ability to the AI to reconnect to the last shunt
    or ai interface they where connected to before.
    The only check that gets skipped doing that, is if that shunt is in
    camera range.
    
    If you ever played AI you know how frustrating it is to have to quickly
    go out of a shunt to do something. Because chances are that you never
    see that shunt again in that round. Probably outside camera range, maybe
    somewhere you don't remember or just moved a little by someone, because
    they wanted to be helpful.
    
    https://github.com/user-attachments/assets/d5040090-2685-4576-8be1-f6272d0f112f
    
    <!-- check boxes for faster reviewing of your PR -->
    
    - [x] I do not require assistance to complete the PR.
    - [x] Before posting/requesting review of a PR, I have verified that the
    changes work.
    - [x] I have added screenshots/videos of the changes, or this PR does
    not change in-game mechanics.
    - [x] I affirm that my changes are licensed under the [MIT
    License](https://github.com/ss14Starlight/space-station-14/blob/Starlight/LICENSE.TXT)
    and grant permission for use in this repository under its conditions.
    
    **Changelog**
    
    :cl: Floximo
    - add: AI Ability to reconnect to the last shunt.
    
    ---------
    
    Co-authored-by: mack.wtf <mackamuir@gmail.com>

--- Content.Shared/_Starlight/Silicons/Borgs/StationAIShuntSystem.cs
+++ Content.Shared/_Starlight/Silicons/Borgs/StationAIShuntSystem.cs
@@ -42,6 +42,7 @@ public sealed class StationAIShuntSystem : EntitySystem
 
         SubscribeLocalEvent<StationAIShuntThroughComponent, GetVerbsEvent<AlternativeVerb>>(GetAltVerbs);
         SubscribeLocalEvent<StationAIShuntThroughComponent, FindShuntTargetEvent>(OnFindShuntTarget);
+        InitializeReconnect();
     }
 
     #region Actions
@@ -50,7 +51,7 @@ public sealed class StationAIShuntSystem : EntitySystem
         if (ev.Handled)
             return;
         var target = ev.Target;
-        if (_vision.IsOutsideCameraViewCached(target))
+        if (!ev.IgnoreCameraView && _vision.IsOutsideCameraViewCached(target))
             return;
 
         // If target has ShuntThrough component, search for a valid target in containers
@@ -98,6 +99,8 @@ public sealed class StationAIShuntSystem : EntitySystem
         _mindSystem.TransferTo(mindId, target);
         shunt.ReturnAction = _actionSystem.AddAction(target, shuntable.UnshuntAction.Id);
         shuntable.Inhabited = target;
+        shuntable.LastShunt = target;
+        Dirty(uid, shuntable);
 
         if (TryComp<SiliconLawProviderComponent>(uid, out var coreLaws))
         {
@@ -308,6 +311,7 @@ public sealed class StationAIShuntSystem : EntitySystem
 
 public sealed partial class AIShuntActionEvent : EntityTargetActionEvent
 {
+    public bool IgnoreCameraView;
 }
 
 public sealed partial class AIUnShuntActionEvent : InstantActionEvent
