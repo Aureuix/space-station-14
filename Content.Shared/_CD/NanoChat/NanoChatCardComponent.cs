24d211f101b0cecb6c6d9c84719db8d5d2c36afb
nykaestra <verityofthestars@gmail.com>
2026-09-09 22:35:48 +1000

    Revert "Added alerts for unread nanochat messages (#5887)"
    This reverts commit 0eef2881

--- Content.Shared/_CD/NanoChat/NanoChatCardComponent.cs
+++ Content.Shared/_CD/NanoChat/NanoChatCardComponent.cs
@@ -1,7 +1,5 @@
 using Content.Shared._CD.CartridgeLoader.Cartridges;
-using Content.Shared.Alert;
 using Robust.Shared.GameStates;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared._CD.NanoChat;
@@ -69,13 +67,4 @@ public sealed partial class NanoChatCardComponent : Component
     /// </summary>
     [DataField]
     public bool ListNumber = true;
-
-    #region Starligt
-
-    /// <summary>
-    /// The alert prototype for unread messages notifications
-    /// </summary>
-    [DataField] public ProtoId<AlertPrototype> Alert = "NanoChatAlert";
-
-    #endregion
 }
