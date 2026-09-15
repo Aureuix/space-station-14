db5f83f7269098d18b52877cf309dcd8ef40192e
DoubleHypercube <development@hypercube.gdn>
2026-09-15 23:59:13 +1200

    Fix damage modifier sets not supporting inheritance

--- Content.Shared/Damage/Prototypes/DamageModifierSetPrototype.cs
+++ Content.Shared/Damage/Prototypes/DamageModifierSetPrototype.cs
@@ -1,19 +1,26 @@
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
 
-namespace Content.Shared.Damage.Prototypes
+namespace Content.Shared.Damage.Prototypes;
+
+/// <summary>
+///     A version of DamageModifierSet that can be serialized as a prototype, but is functionally identical.
+/// </summary>
+/// <remarks>
+///     Done to avoid removing the 'required' tag on the ID and passing around a 'prototype' when we really
+///     just want normal data to be deserialized.
+/// </remarks>
+[Prototype]
+public sealed partial class DamageModifierSetPrototype : DamageModifierSet, IPrototype, IInheritingPrototype
 {
-    /// <summary>
-    ///     A version of DamageModifierSet that can be serialized as a prototype, but is functionally identical.
-    /// </summary>
-    /// <remarks>
-    ///     Done to avoid removing the 'required' tag on the ID and passing around a 'prototype' when we really
-    ///     just want normal data to be deserialized.
-    /// </remarks>
-    [Prototype]
-    public sealed partial class DamageModifierSetPrototype : DamageModifierSet, IPrototype
-    {
-        [ViewVariables]
-        [IdDataField]
-        public string ID { get; private set; } = default!;
-    }
+    [ViewVariables]
+    [IdDataField]
+    public string ID { get; private set; } = default!;
+        
+    [ParentDataField(typeof(AbstractPrototypeIdArraySerializer<DamageModifierSetPrototype>))]
+    public string[]? Parents { get; private set; }
+        
+    [NeverPushInheritance]
+    [AbstractDataField]
+    public bool Abstract { get; private set; }
 }
