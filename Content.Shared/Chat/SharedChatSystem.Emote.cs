c20f0f4a0ce62529fc1d7f60d8e7c62d6946d1b1
Avris <318705253+Wurbles@users.noreply.github.com>
2026-09-17 07:06:52 -0400

    Emote Trigger Leniency
    Emotes can now trigger on the first word in an emote even if more words follow.

--- Content.Shared/Chat/SharedChatSystem.Emote.cs
+++ Content.Shared/Chat/SharedChatSystem.Emote.cs
@@ -169,7 +169,7 @@ public abstract partial class SharedChatSystem
     /// <returns>True if the chat message should be displayed (because the emote was explicitly cancelled), false if it should not be.</returns>
     protected bool TryEmoteChatInput(EntityUid source, string textInput)
     {
-        var actionTrimmedLower = TrimPunctuation(textInput.ToLower());
+        var actionTrimmedLower = TrimPunctuation(textInput.ToLower().Split(' ')[0]);
         if (!_wordEmoteDict.TryGetValue(actionTrimmedLower, out var emote))
             return true;
 
